22474b3346b57f00d0e2cda3a80ff51f205c1e1f
jreleaserbot <jreleaser@kordamp.org>
2022-11-29 10:20:22 +0100

    Bump just to 0.3.0.

--- Formula/just.rb
+++ Formula/just.rb
@@ -1,9 +1,7 @@
-require_relative "../custom_download_strategy.rb"
-
 class Just < Formula
   desc "Command Line Spring Boot runner"
   homepage "https://github.com/maciejwalkowiak/just"
-  url "https://github.com/maciejwalkowiak/just/releases/download/0.3.0/just-0.3.0-osx-x86_64.zip", :using => CustomGitHubPrivateRepositoryReleaseDownloadStrategy
+  url "https://github.com/maciejwalkowiak/just/releases/download/0.3.0/just-0.3.0-osx-x86_64.zip"
   version "0.3.0"
   sha256 "3ede2cade395166917d0d3b1651b65203c85a41444904b145ac5863c8d720f52"
   license "Not OSS"
