bd737497512bc3ac3f1d43e6a24a0055e8a1325d
jreleaserbot <jreleaser@kordamp.org>
2022-11-29 16:47:30 +0000

    Bump just to 0.3.0.

--- Formula/just.rb
+++ Formula/just.rb
@@ -3,18 +3,11 @@ class Just < Formula
   homepage "https://github.com/maciejwalkowiak/just"
   url "https://github.com/maciejwalkowiak/just/releases/download/0.3.0/just-0.3.0-osx-x86_64.zip"
   version "0.3.0"
-  sha256 "3ede2cade395166917d0d3b1651b65203c85a41444904b145ac5863c8d720f52"
+  sha256 "ebc4321ca106c5ef429c7bad33efd954dd9dfc78d5c926fa636d837d15191f6a"
   license "Not OSS"
-
-  depends_on "maven"
 
 
   def install
-  	# for whatever reason this does not work:
-  	# system "mvn", "org.apache.maven.plugins:maven-install-plugin:3.0.1:install-file", "-Dfile=fly-devcontainers.jar"
-  	cmd = "mvn org.apache.maven.plugins:maven-install-plugin:3.0.1:install-file -Dfile=fly-devcontainers.jar"
-  	value = `#{cmd}`
-  	print value
     bin.install "just"
   end
 
