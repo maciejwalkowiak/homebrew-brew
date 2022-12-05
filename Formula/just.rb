b0a420fe93d8d8386c2c1b333b6f21ad4fd4bfb5
jreleaserbot <jreleaser@kordamp.org>
2022-12-05 16:30:02 +0000

    Bump just to 0.6.0.

--- Formula/just.rb
+++ Formula/just.rb
@@ -1,9 +1,9 @@
 class Just < Formula
   desc "Command Line Spring Boot runner"
   homepage "https://github.com/maciejwalkowiak/just"
-  url "https://github.com/maciejwalkowiak/just/releases/download/0.5.0/just-0.5.0-osx-x86_64.zip"
-  version "0.5.0"
-  sha256 "eb11a086495ae39e882f38ad5d35e81e046d771369881a804456ba454e44400a"
+  url "https://github.com/maciejwalkowiak/just/releases/download/0.6.0/just-0.6.0-osx-x86_64.zip"
+  version "0.6.0"
+  sha256 "965fdb33312efe5109156eff3841979462e455d8b05e107a4361be771b73af60"
   license "Apache License, Version 2.0"
 
 
