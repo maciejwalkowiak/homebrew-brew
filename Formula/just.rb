fd5a60d87ee5e4b9b6354b44e914a083b888a2ac
jreleaserbot <jreleaser@kordamp.org>
2022-12-09 09:06:23 +0000

    Bump just to 0.9.0.

--- Formula/just.rb
+++ Formula/just.rb
@@ -1,14 +1,20 @@
 class Just < Formula
   desc "Command Line Spring Boot runner"
   homepage "https://github.com/maciejwalkowiak/just"
-  url "https://github.com/maciejwalkowiak/just/releases/download/0.8.0/just-0.8.0-osx-x86_64.zip"
-  version "0.8.0"
-  sha256 "67336c72091173e0d2f9941f21595bd473dc5b791c3c0141729f71caaadf1793"
+  url "https://github.com/maciejwalkowiak/just/releases/download/0.9.0/just-0.9.0-osx-x86_64.zip"
+  version "0.9.0"
+  sha256 "f75a1ad4d5e5195000f633996c97e4b0a6668d9a0dcfd2f78f82d87c5835b883"
   license "Apache License, Version 2.0"
 
 
   def install
     bin.install "just"
+  end
+
+  def caveats
+    <<~EOS
+	  Just collects anonymous usage data. If you don't agree, just don't use it.
+	EOS
   end
 
   test do
